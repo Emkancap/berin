b812a2ab6197d858d1fd661fa5de34291aa1bf7b
unknown <bravotech0sa@gmail.com>
2022-11-25 22:46:46 +0300

    upload

---
+++ src/SignUp.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+export default function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordR, setPasswordR] = useState("");
+  const [accept, setAccept] = useState(false);
+
+  function Submit(e) {
+    e.preventDefault();
+    setAccept(true);
+  }
+  return (
+    <div className="parent">
+      <div className="register">
+        <form onSubmit={Submit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Name..."
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email..."
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {password.length < 8 && accept && (
+            <p>Password must be more than 8 Char</p>
+          )}
+          <label htmlFor="repeat">Repeat Password:</label>
+          <input
+            type="password"
+            id="repeat"
+            placeholder="Repeat Password..."
+            value={passwordR}
+            onChange={(e) => setPasswordR(e.target.value)}
+          />
+          {passwordR !== password && accept && <p>Password dose not match</p>}
+
+          <div style={{ textAlign: "center" }}>
+            <button type="submit">Register</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
